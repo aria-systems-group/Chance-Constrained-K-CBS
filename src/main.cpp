97afc5a0945704c71a1a2d4dae433e64c428765d
Justin Kottinger <justin.kottinger@colorado.edu>
2021-09-27 10:50:20 -0600

    update main file with all changes.

--- src/main.cpp
+++ src/main.cpp
@@ -5,46 +5,31 @@
 *********************************************************************/
  
 /* Author: Justin Kottinger */
-#include "../includes/OdeFunctions.h"
+
 #include "../includes/World.h"
-#include "../includes/OdeFunctions.h"
-#include "../includes/Goals.h"
-#include "../includes/collisionChecking.h"
 #include "../includes/multiAgentSetUp.h"
+#include "../includes/KD_CBS.h"
 
 
-// this follows syntax of printf see (https://www.cplusplus.com/reference/cstdio/printf/)
+namespace ob = ompl::base;
+namespace oc = ompl::control;
+
 // OMPL_INFORM("OMPL version: %s", OMPL_VERSION);  // blue font
 // OMPL_WARN("OMPL version: %s", OMPL_VERSION);  // yellow font
 // OMPL_ERROR("OMPL version: %s", OMPL_VERSION); // red font
+// these follow syntax of printf see (https://www.cplusplus.com/reference/cstdio/printf/)
  
 int main(int argc, char ** argv)
 {
     std::string problem = argv[1];
     World *w = yaml2world(problem);
-    std::vector<oc::SimpleSetup> allAgentSetUp = multiAgentSimpleSetUp(w);
+    const std::vector<oc::SimpleSetup> allAgentSetUp = multiAgentSimpleSetUp(w);
+    oc::KD_CBS *p = new oc::KD_CBS(allAgentSetUp);
+    p->setWorld(w);
+    ob::PlannerPtr planner(p);
     OMPL_INFORM("Set-Up Complete. Press ENTER to begin planning.");
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-
-    for (oc::SimpleSetup s: allAgentSetUp)
-    {
-        ob::PlannerStatus solved = s.solve(10.0);
-        if (solved)
-        {
-            std::cout << "Found solution:" << std::endl;
-            s.getSolutionPath().printAsMatrix(std::cout);
-        }
-        else
-            std::cout << "No solution found" << std::endl;
-    }
+    
+    planner->solve(1.0);
     return 0;
 }
-
-
-
-
-
-
-
-
-

df3fea246651824845414c2101debc23f5238541
Justin Kottinger <justin.kottinger@colorado.edu>
2022-02-13 08:46:08 -0700

    added arbitrary composed system behaviour

--- src/includes/Goals.h
+++ src/includes/Goals.h
@@ -74,7 +74,7 @@ private:
     std::vector<double> goal_;
     double toll_;
 };
-/********* END 3D Goal Class *********/
+/********* END 2D Goal Class *********/
 
 /********* Arbirtrary 3D Goal Class *********/
 // Defines a spherical goal region centered at goal, with radius toll
@@ -115,3 +115,152 @@ private:
     double toll_;
 };
 /********* END 3D Goal Class *********/
+
+/********* Arbirtrary Composed 2D Goal Class *********/
+// Defines a circular goal region centered at goal, with radius toll
+class ArbirtryComposedGoal_2D : public ompl::base::Goal
+{
+public:
+    ArbirtryComposedGoal_2D(const oc::SpaceInformationPtr &si, 
+        std::vector<double> goal, const double toll, const std::string dynModel) : 
+            ompl::base::Goal(si), goal_{goal}, toll_{toll}, dyn_{dynModel}
+    {
+    }
+
+    virtual bool isSatisfied(const ob::State *st) const
+    {
+        auto compState = st->as<ompl::base::CompoundStateSpace::StateType>();
+        std::vector<std::vector<double>> poses;
+        int numVs = 0;
+        if (dyn_ == "Two Dynamic Cars")
+            numVs = 2;
+        else if (dyn_ == "Three Dynamic Cars")
+            numVs = 3;
+        
+        if (dyn_ == "Two Dynamic Cars" || dyn_ == "Three Dynamic Cars")
+        {
+            for (int a = 0; a < numVs; a++)
+            {
+                auto xyState = compState->as<
+                    ob::RealVectorStateSpace::StateType>(2*a + 0);
+                std::vector<double> xy_pos{xyState->values[0], xyState->values[1]};
+                poses.push_back(xy_pos);
+            }
+        }
+        else
+            OMPL_ERROR("Composed Goal not implemented for current composed dynamics.");
+        
+        if (poses.size() != numVs)
+            OMPL_ERROR("Goal checking not working as expected.");
+;
+        // given a pos for each agent, calc if all in goal
+        for (int a = 0; a < poses.size(); a++)
+        {
+            const double dx_sq = pow(goal_[poses.size() * a] - poses[a][0], 2);
+            const double dy_sq = pow(goal_[poses.size() * a + 1] - poses[a][1], 2);
+            // if a single agent is out of goal, system is not in goal
+            if (sqrt(dx_sq + dy_sq) > toll_)
+                return false;
+        }
+
+        // survived the pos check through all agents, found goal
+        return true;
+    }
+
+    // Is a state in a goal region, also fill "distance" with result
+    virtual bool isSatisfied(const ob::State *st, double *distance) const
+    {
+        auto compState = st->as<ompl::base::CompoundStateSpace::StateType>();
+        std::vector<std::vector<double>> poses;
+        int numVs = 0;
+        if (dyn_ == "Two Dynamic Cars")
+            numVs = 2;
+        else if (dyn_ == "Three Dynamic Cars")
+            numVs = 3;
+
+        if (dyn_ == "Two Dynamic Cars" || dyn_ == "Three Dynamic Cars")
+        {
+            for (int a = 0; a < numVs; a++)
+            {
+                auto xyState = compState->as<
+                    ob::RealVectorStateSpace::StateType>(2*a + 0);
+                std::vector<double> xy_pos{xyState->values[0], xyState->values[1]};
+                poses.push_back(xy_pos);
+            }
+        }
+        else
+            OMPL_ERROR("Composed Goal not implemented for current composed dynamics.");
+        
+        /* Nothing passed here should change */
+        if (poses.size() != numVs)
+            OMPL_ERROR("Goal checking not working as expected.");
+
+        // given a pos for each agent, calc if all in goal
+        double dist = 0.0;
+        bool isSolved = true;
+        for (int a = 0; a < poses.size(); a++)
+        {
+            const double dx_sq = pow(goal_[poses.size() * a] - poses[a][0], 2);
+            const double dy_sq = pow(goal_[poses.size() * a + 1] - poses[a][1], 2);
+            dist += sqrt(dx_sq + dy_sq);
+            // if a single agent is out of goal, system is not in goal
+            if (sqrt(dx_sq + dy_sq) > toll_)
+            {
+                isSolved = false;
+            }
+        }
+
+        // const double d = getDistance2D(st);
+        distance = &dist;
+
+        // survived the pos check through all agents, found goal
+        return isSolved;
+    }
+
+    std::vector<int> isInGoal(const ob::State *st)
+    {
+        auto compState = st->as<ompl::base::CompoundStateSpace::StateType>();
+        std::vector<std::vector<double>> poses;
+        int numVs = 0;
+        if (dyn_ == "Two Dynamic Cars")
+            numVs = 2;
+        else if (dyn_ == "Three Dynamic Cars")
+            numVs = 3;
+
+        if (dyn_ == "Two Dynamic Cars" || dyn_ == "Three Dynamic Cars")
+        {
+            for (int a = 0; a < numVs; a++)
+            {
+                auto xyState = compState->as<
+                    ob::RealVectorStateSpace::StateType>(2*a + 0);
+                std::vector<double> xy_pos{xyState->values[0], xyState->values[1]};
+                poses.push_back(xy_pos);
+            }
+        }
+        else
+            OMPL_ERROR("Composed Goal not implemented for current composed dynamics.");
+        
+        /* Nothing passed here should change */
+        if (poses.size() != numVs)
+            OMPL_ERROR("Goal checking not working as expected.");
+
+        // given a pos for each agent, calc if all in goal
+        std::vector<int> idxSolved;
+        for (int a = 0; a < poses.size(); a++)
+        {
+            const double dx_sq = pow(goal_[poses.size() * a] - poses[a][0], 2);
+            const double dy_sq = pow(goal_[poses.size() * a + 1] - poses[a][1], 2);
+            double dist = sqrt(dx_sq + dy_sq);
+            // if a single agent is out of goal, system is not in goal
+            if (dist < toll_)
+                idxSolved.push_back(a);
+        }
+        return idxSolved;
+    }
+
+private:
+    std::vector<double> goal_;
+    double toll_;
+    const std::string dyn_;
+};
+/********* END 2D Composed Goal Class *********/
--- src/includes/collisionChecking.h
+++ src/includes/collisionChecking.h
@@ -35,10 +35,10 @@ typedef boost::geometry::model::polygon<point> polygon;
 
 /********* Definition of 2D State Validity Checker *********/
 // this class works for all dynamic models of form x = [x, y, theta, ...]
-class isStateValid_2D_Test : public ob::StateValidityChecker
+class isStateValid_2D : public ob::StateValidityChecker
 {
 public:
-    isStateValid_2D_Test(const oc::SpaceInformationPtr &si, const World *w, const Agent *a) :
+    isStateValid_2D(const oc::SpaceInformationPtr &si, const World *w, const Agent *a) :
         ob::StateValidityChecker(si)
         {
             si_ = si.get();
@@ -91,6 +91,18 @@ public:
             if (! boost::geometry::disjoint(agent, o.poly_))
                 return false;
         }
+        // create polygon map
+        std::vector<point> map_pts{
+        point(0,0),
+        point(0,w_->getWorldDimensions()[1]),
+        point(w_->getWorldDimensions()[0], w_->getWorldDimensions()[1]),
+        point(w_->getWorldDimensions()[0], 0),
+        point(0,0)};
+        polygon map;
+        boost::geometry::assign_points(map, map_pts);
+        // verify that agent is completely inside another
+        if (! boost::geometry::within(agent, map))
+            return false;
         return true;
     }
 
@@ -104,10 +116,10 @@ private:
 
 /********* Definition of 3D State Validity Checker *********/
 // this class works for all dynamic models of form x = [x, y, z, theta, ...]
-class isStateValid_3D_Test : public ob::StateValidityChecker
+class isStateValid_3D : public ob::StateValidityChecker
 {
 public:
-    isStateValid_3D_Test(const oc::SpaceInformationPtr &si, const World *w, const Agent *a, const std::vector<int> constraints = {}) :
+    isStateValid_3D(const oc::SpaceInformationPtr &si, const World *w, const Agent *a, const std::vector<int> constraints = {}) :
         ob::StateValidityChecker(si)
         {
             si_ = si.get();
@@ -178,3 +190,134 @@ private:
     const Agent *a_;
     std::vector<int> constraints_;
 };
+
+/********* Definition of Composed 2D State Validity Checker *********/
+// this class works for all composed dynamic models of form x = [x, y, theta, ...]
+class isComposedStateValid_2D : public ob::StateValidityChecker
+{
+public:
+    isComposedStateValid_2D(const oc::SpaceInformationPtr &si, 
+        const World *w, const Agent *a, const std::string dynModel) :
+        ob::StateValidityChecker(si), si_{si.get()}, w_{w}, a_{a}, dyn_{dynModel}
+        {
+        }
+ 
+    virtual bool isValid(const ob::State *state) const
+    {
+        if (!si_->satisfiesBounds(state))
+            return false;
+
+        // create polygon map
+        std::vector<point> map_pts{
+        point(0,0),
+        point(0,w_->getWorldDimensions()[1]),
+        point(w_->getWorldDimensions()[0], w_->getWorldDimensions()[1]),
+        point(w_->getWorldDimensions()[0], 0),
+        point(0,0)};
+        polygon map;
+        boost::geometry::assign_points(map, map_pts);
+
+        // use comp state to fill vehicles vector
+        auto compState = state->as<ob::CompoundStateSpace::StateType>();
+        std::vector<polygon> vehicles;
+        
+        int numVs = 0;
+        if (dyn_ == "Two Dynamic Cars" )
+            numVs = 2;
+        else if (dyn_ == "Three Dynamic Cars" )
+            numVs = 3;
+
+        
+        if (dyn_ == "Two Dynamic Cars" || dyn_ == "Three Dynamic Cars")
+        {
+            for (int a = 0; a < numVs; a++)
+            {
+                auto xyState = compState->as<
+                    ob::RealVectorStateSpace::StateType>(2*a + 0);
+                const double cx = xyState->values[0];
+                const double cy = xyState->values[1];
+                const double theta = compState->as<
+                    ob::SO2StateSpace::StateType>(2*a + 1)->value;
+
+                // Get important params from car object
+                const double carWidth = a_->getShape()[0];
+                const double carHeight = a_->getShape()[1];
+
+                // turn (x,y, theta), width, length to a polygon object
+                // see https://stackoverflow.com/questions/41898990/find-corners-of-a-rotated-rectangle-given-its-center-point-and-rotation
+                // TOP RIGHT VERTEX:
+                const double TR_x = cx + ((carWidth / 2) * cos(theta)) - ((carHeight / 2) * sin(theta));
+                const double TR_y = cy + ((carWidth / 2) * sin(theta)) + ((carHeight / 2) * cos(theta));
+                std::string top_right = std::to_string(TR_x) + " " + std::to_string(TR_y);
+                // TOP LEFT VERTEX:
+                const double TL_x = cx - ((carWidth / 2) * cos(theta)) - ((carHeight / 2) * sin(theta));
+                const double TL_y = cy - ((carWidth / 2) * sin(theta)) + ((carHeight / 2) * cos(theta));
+                std::string top_left = std::to_string(TL_x) + " " + std::to_string(TL_y);
+                // BOTTOM LEFT VERTEX:
+                const double BL_x = cx - ((carWidth / 2) * cos(theta)) + ((carHeight / 2) * sin(theta));
+                const double BL_y = cy - ((carWidth / 2) * sin(theta)) - ((carHeight / 2) * cos(theta));
+                std::string bottom_left = std::to_string(BL_x) + " " + std::to_string(BL_y);
+                // BOTTOM RIGHT VERTEX:
+                const double BR_x = cx + ((carWidth / 2) * cos(theta)) + ((carHeight / 2) * sin(theta));
+                const double BR_y = cy + ((carWidth / 2) * sin(theta)) - ((carHeight / 2) * cos(theta));
+                std::string bottom_right = std::to_string(BR_x) + " " + std::to_string(BR_y);
+
+                // convert to string for easy initializataion
+                std::string points = "POLYGON((" + bottom_left + "," + bottom_right + "," + top_right + "," + top_left + "," + bottom_left + "))";
+                polygon agent;
+                boost::geometry::read_wkt(points,agent);
+
+                vehicles.push_back(agent);
+            }
+        }
+        else
+            OMPL_ERROR("Current composed dynamics model is not implemented.");
+
+        if (vehicles.size() != numVs)
+        {
+            printf("num polys: %lu, num vs: %i \n", vehicles.size(), numVs);
+            OMPL_ERROR("Collision Checking not working as expected.");
+        }
+        
+        // check agent is disjoint from all obstacles and map
+        for (const auto v: vehicles)
+        {
+            // check against obs
+            for (Obstacle o: w_->getObstacles())
+            {
+                if (! boost::geometry::disjoint(v, o.poly_))
+                {
+                    return false;
+                }
+            }
+            // check against map
+            if (! boost::geometry::within(v, map))
+            {
+                return false;
+            }
+        }
+        // check vehicles against each other
+        for (int a1 = 0; a1 < vehicles.size(); a1++)
+        {
+            for (int a2 = 0; a2 < vehicles.size(); a2++)
+            {
+                if (a1 != a2)
+                {
+                    if (! boost::geometry::disjoint(vehicles[a1], vehicles[a2]))
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+private:
+    const ob::SpaceInformation *si_;
+    const World *w_;
+    const Agent *a_;
+    const std::string dyn_;
+    
+};
+/********* END 2D Composed State Validity Checker *********/

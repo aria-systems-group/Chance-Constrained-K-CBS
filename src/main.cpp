831c9b3de088b7cbf103b9a9e349e1b2306dc4fd
Justin Kottinger <justin.kottinger@colorado.edu>
2022-02-16 06:37:25 -0700

    added benchmark functionality

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "includes/pbs.h"
 #include "includes/postProcess.h"
 #include "includes/MA_RRT.h"
+#include "includes/Benchmark.h"
 #include <ompl/control/planners/rrt/RRT.h>
 
 
@@ -21,17 +22,18 @@ namespace oc = ompl::control;
 // OMPL_WARN("OMPL version: %s", OMPL_VERSION);  // yellow font
 // OMPL_ERROR("OMPL version: %s", OMPL_VERSION); // red font
 // these follow syntax of printf see (https://www.cplusplus.com/reference/cstdio/printf/)
+
  
 int main(int argc, char ** argv)
 {
     std::string problem = argv[1];
-    World *w = yaml2world(problem);
-    const std::vector<std::pair<std::shared_ptr<oc::SpaceInformation>, 
-        std::shared_ptr<ob::ProblemDefinition>>> mmpp = multiAgentSetUp(w);
     
     std::string planner = argv[2];
     if (planner == "K-CBS")
     {
+        World *w = yaml2world(problem);
+        const std::vector<std::pair<std::shared_ptr<oc::SpaceInformation>, 
+            std::shared_ptr<ob::ProblemDefinition>>> mmpp = multiAgentSetUp(w);
         oc::KD_CBS *p = new oc::KD_CBS(mmpp); // K_CBS
         p->setWorld(w);
         ob::PlannerPtr planner(p);
@@ -50,19 +52,17 @@ int main(int argc, char ** argv)
                 plan.push_back(traj);
             }
             std::vector<int> expCosts = generateExplanation(plan);
-            // for (auto m: mmpp)
-            // {
-            //     oc::PathControl traj = static_cast<oc::PathControl &>
-            //         (*m.second->getSolutionPath());
-            //     traj.printAsMatrix(std::cout);
-            // }
             problem.resize(problem.size() - 4); // remove ".yml"
             printf("Writing Solution to %s \n", problem.c_str());
             write2sys(plan, w->getAgents(), problem, expCosts);
         }
+        p->freeMemory();
     }
     else if (planner == "PBS")
     {
+        World *w = yaml2world(problem);
+        const std::vector<std::pair<std::shared_ptr<oc::SpaceInformation>, 
+            std::shared_ptr<ob::ProblemDefinition>>> mmpp = multiAgentSetUp(w);
         oc::PBS *p = new oc::PBS(mmpp); // PBS
         p->setWorld(w);
         ob::PlannerPtr planner(p);
@@ -89,6 +89,9 @@ int main(int argc, char ** argv)
     }
     else if (planner == "RRT")
     {
+        World *w = yaml2world(problem);
+        const std::vector<std::pair<std::shared_ptr<oc::SpaceInformation>, 
+            std::shared_ptr<ob::ProblemDefinition>>> mmpp = multiAgentSetUp(w);
         OMPL_WARN("Using centralized RRT currently only plans for the first agent in the problem definition. This is only a problem if you provided a yaml file that contains multiple agents.\n");
         oc::RRT *p = new oc::RRT(mmpp[0].first); // RRT for centralized planning
         p->setProblemDefinition(mmpp[0].second);
@@ -115,6 +118,9 @@ int main(int argc, char ** argv)
     }
     else if (planner == "MA-RRT")
     {
+        World *w = yaml2world(problem);
+        const std::vector<std::pair<std::shared_ptr<oc::SpaceInformation>, 
+            std::shared_ptr<ob::ProblemDefinition>>> mmpp = multiAgentSetUp(w);
         OMPL_WARN("Using centralized RRT currently only plans for the first agent in the problem definition. This is only a problem if you provided a yaml file that contains multiple agents.\n");
         oc::MA_RRT *p = new oc::MA_RRT(mmpp[0].first); // MA-RRT for centralized planning
         p->setProblemDefinition(mmpp[0].second);
@@ -136,6 +142,21 @@ int main(int argc, char ** argv)
             write2sys(plan, w->getAgents(), problem, expCosts);
         }
     }
+    else if (planner == "Benchmark")
+    {
+        const char* logName;
+        if (argc == 4)
+            logName = argv[3];
+        else
+        {
+            OMPL_WARN("No output file given. Will output to no_name.csv");
+            logName = "no_name.csv";
+        }
+        const dataStruct results = benchmark(problem, 300);
+        write_csv(logName, results);
+    }
+    else
+        printf("No propper planner given. No planning will be performed. \n");
     
     return 0;
 }
